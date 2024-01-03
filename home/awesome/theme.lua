9d1c0f164ae730d1bc7f3bdaf0bc0b2cd252a7bf
Julian <julian.ollivieira@gmail.com>
2024-01-03 22:25:40 +0100

    add mem and fs widgets to top bar

--- home/awesome/theme.lua
+++ home/awesome/theme.lua
@@ -36,10 +36,30 @@ local dtwidget = wibox.container.margin(dtwidget, 0, dpi(padding), dpi(padding),
 
 -- cpu
 local cputext = wibox.widget.textbox()
-local cpuwidget = wibox.container.margin(cputext, dpi(padding * 2), dpi(padding * 2))
-local cpuwidget = wibox.container.background(cpuwidget, theme.bg_focus, gears.shape.rectangle)
-local cpuwidget = wibox.container.margin(cpuwidget, 0, dpi(padding), dpi(padding), dpi(padding))
+-- local cpuwidget = wibox.container.margin(cputext, dpi(padding * 2), dpi(padding * 2))
+-- local cpuwidget = wibox.container.background(cpuwidget, theme.bg_focus, gears.shape.rectangle)
+-- local cpuwidget = wibox.container.margin(cpuwidget, 0, dpi(padding), dpi(padding), dpi(padding))
 vicious.register(cputext, vicious.widgets.cpu, "<span font-size='" .. theme.c_font_size .. "' font='" .. theme.fontname .. "'><b>cpu $1%</b></span>", 1)
+
+-- mem
+local memtext = wibox.widget.textbox()
+-- local memwidget = wibox.container.margin(memtext, dpi(padding * 2), dpi(padding * 2))
+-- local memwidget = wibox.container.background(memwidget, theme.bg_focus, gears.shape.rectangle)
+-- local memwidget = wibox.container.margin(memwidget, 0, dpi(padding), dpi(padding), dpi(padding))
+vicious.register(memtext, vicious.widgets.mem, "<span font-size='" .. theme.c_font_size .. "' font='" .. theme.fontname .. "'><b>mem $1%</b></span>", 1)
+
+-- fs
+local fstext = wibox.widget.textbox()
+-- local fswidget = wibox.container.margin(fstext, dpi(padding * 2), dpi(padding * 2))
+-- local fswidget = wibox.container.background(fswidget, theme.bg_focus, gears.shape.rectangle)
+-- local fswidget = wibox.container.margin(fswidget, 0, dpi(padding), dpi(padding), dpi(padding))
+vicious.register(fstext, vicious.widgets.fs, "<span font-size='" .. theme.c_font_size .. "' font='" .. theme.fontname .. "'><b>fs ${/ used_p}%</b></span>", 1)
+
+-- cpu, mem and fs widget
+local cpumemfswidget = wibox.widget { cputext, bar, memtext, bar, fstext, spacing = 5, layout = wibox.layout.fixed.horizontal }
+local cpumemfswidget = wibox.container.margin(cpumemfswidget, dpi(padding * 2), dpi(padding * 2))
+local cpumemfswidget = wibox.container.background(cpumemfswidget, theme.bg_focus, gears.shape.rectangle)
+local cpumemfswidget = wibox.container.margin(cpumemfswidget, 0, dpi(padding), dpi(padding), dpi(padding))
 
 function theme.at_screen_connect(s)
     -- quake application
@@ -72,7 +92,10 @@ function theme.at_screen_connect(s)
 				nil,
         {
             layout = wibox.layout.fixed.horizontal,
-						cpuwidget,
+						cpumemfswidget,
+						-- fswidget,
+						-- memwidget,
+						-- cpuwidget,
 						dtwidget,
             -- datewidget,
 						-- bar,
